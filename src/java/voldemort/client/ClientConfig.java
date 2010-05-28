b20d40a817a6158b9254df836fbe6b264bef1295
Alex Feinberg <alex@strlen.net>
2010-05-28 01:11:10 -0700

    Added an option for hinted handoff to ClientConfig.

--- src/java/voldemort/client/ClientConfig.java
+++ src/java/voldemort/client/ClientConfig.java
@@ -55,6 +55,8 @@ public class ClientConfig {
 
     private volatile boolean enablePipelineRoutedStore = false;
 
+    private volatile boolean enableHintedHandoff = false;
+
     private volatile String failureDetectorImplementation = FailureDetectorConfig.DEFAULT_IMPLEMENTATION_CLASS_NAME;
     private volatile long failureDetectorBannagePeriod = FailureDetectorConfig.DEFAULT_BANNAGE_PERIOD;
     private volatile int failureDetectorThreshold = FailureDetectorConfig.DEFAULT_THRESHOLD;
@@ -87,6 +89,7 @@ public class ClientConfig {
     public static final String REQUEST_FORMAT_PROPERTY = "request_format";
     public static final String ENABLE_JMX_PROPERTY = "enable_jmx";
     public static final String ENABLE_PIPELINE_ROUTED_STORE_PROPERTY = "enable_pipeline_routed_store";
+    public static final String ENABLE_HINTED_HANDOFF_PROPERTY = "enable_hinted_handoff";
     public static final String FAILUREDETECTOR_IMPLEMENTATION_PROPERTY = "failuredetector_implementation";
     public static final String FAILUREDETECTOR_BANNAGE_PERIOD_PROPERTY = "failuredetector_bannage_period";
     public static final String FAILUREDETECTOR_THRESHOLD_PROPERTY = "failuredetector_threshold";
@@ -158,6 +161,9 @@ public class ClientConfig {
 
         if(props.containsKey(ENABLE_PIPELINE_ROUTED_STORE_PROPERTY))
             this.setEnablePipelineRoutedStore(props.getBoolean(ENABLE_PIPELINE_ROUTED_STORE_PROPERTY));
+
+        if(props.containsKey(ENABLE_HINTED_HANDOFF_PROPERTY))
+            this.setEnableHintedHandoff(props.getBoolean(ENABLE_HINTED_HANDOFF_PROPERTY));
 
         if(props.containsKey(FAILUREDETECTOR_IMPLEMENTATION_PROPERTY))
             this.setFailureDetectorImplementation(props.getString(FAILUREDETECTOR_IMPLEMENTATION_PROPERTY));
@@ -472,8 +478,17 @@ public class ClientConfig {
         return enablePipelineRoutedStore;
     }
 
+    public boolean isHintedHandoffEnabled() {
+        return enableHintedHandoff;
+    }
+
     public ClientConfig setEnablePipelineRoutedStore(boolean enablePipelineRoutedStore) {
         this.enablePipelineRoutedStore = enablePipelineRoutedStore;
+        return this;
+    }
+
+    public ClientConfig setEnableHintedHandoff(boolean enableHintedHandoff) {
+        this.enableHintedHandoff = enableHintedHandoff;
         return this;
     }
 
