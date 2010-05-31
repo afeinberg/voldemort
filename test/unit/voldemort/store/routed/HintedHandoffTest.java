2f55bf60b46002605b78057445d072308650e4bf
Alex Feinberg <alex@strlen.net>
2010-05-31 16:17:37 -0700

    Created a failing test for hinted handoff.

--- test/unit/voldemort/store/routed/HintedHandoffTest.java
+++ test/unit/voldemort/store/routed/HintedHandoffTest.java
@@ -3,8 +3,11 @@ package voldemort.store.routed;
 import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Multimap;
+import com.google.common.collect.Sets;
+import org.apache.log4j.Logger;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -28,9 +31,7 @@ import voldemort.store.memory.InMemoryStorageEngine;
 import voldemort.utils.ByteArray;
 import voldemort.versioning.Versioned;
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -40,9 +41,11 @@ import static org.junit.Assert.*;
 @RunWith(Parameterized.class)
 public class HintedHandoffTest {
 
+    private final static Logger logger = Logger.getLogger(HintedHandoffTest.class);
     private final static String STORE_NAME = "test";
     private final static int NUM_THREADS = 4;
     private final static int NUM_NODES = 9;
+    private final static int NUM_FAILED_NODES = 6;
     private final static int KEY_LENGTH = 512;
     private final static int VALUE_LENGTH = 1024;
 
@@ -98,7 +101,11 @@ public class HintedHandoffTest {
             routedStoreThreadPool.shutdown();
     }
 
+    /**
+     * This is a known failing test. Will pass once hinted handoff is implemented.
+     */
     @Test
+    @Ignore
     public void testHintedHandOff() throws Exception {
         RoutedStore routedStore = routedStoreFactory.create(cluster,
                                                             storeDef,
@@ -122,11 +129,45 @@ public class HintedHandoffTest {
             keyValues.put(randomKey, randomValue);
         }
 
-        for (ByteArray key: keysToNodes.values()) {
-            Versioned<byte[]> versioned = new Versioned<byte[]>(keyValues.get(key));
-            routedStore.put(key, versioned);
+        Set<Integer> failedNodes = Sets.newHashSet();
+        Random rand = new Random();
+        while (failedNodes.size() < NUM_FAILED_NODES) {
+            int i = rand.nextInt(NUM_NODES);
+            failedNodes.add(i);
         }
 
+        for (int nodeId: failedNodes) {
+            ForceFailStore forceFailStore = getForceFailStore(nodeId);
+            forceFailStore.setFailAll(true);
+            logger.info("Started failing requests to " + nodeId);
+        }
+
+        Map<Integer,Exception> exceptions = Maps.newHashMap();
+        while (exceptions.size() < 1) {
+            for (Map.Entry<Integer, ByteArray> entry: keysToNodes.entries()) {
+                int nodeId = entry.getKey();
+                ByteArray key = entry.getValue();
+                try {
+                    Versioned<byte[]> versioned = new Versioned<byte[]>(keyValues.get(key));
+                    routedStore.put(key, versioned);
+                } catch (Exception e) {
+                    exceptions.put(nodeId, e);
+                }
+            }
+        }
+
+        for (int nodeId: failedNodes) {
+            ForceFailStore forceFailStore = getForceFailStore(nodeId);
+            forceFailStore.setFailAll(false);
+            logger.info("Stopped failing requests to " + nodeId);
+        }
+
+        for (Map.Entry<ByteArray, byte[]> entry: keyValues.entrySet()) {
+            List<Versioned<byte[]>> versionedValues = routedStore.get(entry.getKey());
+
+            assertTrue(versionedValues.size() > 0);
+            assertEquals(entry.getValue(), versionedValues.get(0).getValue());
+        }
     }
 
     public ForceFailStore<ByteArray, byte[]> getForceFailStore(int nodeId) {
