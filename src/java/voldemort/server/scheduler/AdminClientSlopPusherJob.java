51a8a6d88aae6208c86109e151b66c283e4595df
Alex Feinberg <alex@strlen.net>
2010-10-29 18:24:39 -0700

    Checkpoint

--- src/java/voldemort/server/scheduler/AdminClientSlopPusherJob.java
+++ src/java/voldemort/server/scheduler/AdminClientSlopPusherJob.java
@@ -1,12 +1,14 @@
 package voldemort.server.scheduler;
 
 import com.google.common.collect.AbstractIterator;
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.ListMultimap;
 import com.google.common.collect.Maps;
+import com.google.common.collect.Multimap;
 import org.apache.log4j.Logger;
 import voldemort.client.protocol.admin.AdminClient;
 import voldemort.client.protocol.admin.AdminClientConfig;
 import voldemort.cluster.Cluster;
-import voldemort.cluster.failuredetector.FailureDetector;
 import voldemort.server.StoreRepository;
 import voldemort.store.StorageEngine;
 import voldemort.store.slop.Slop;
@@ -16,9 +18,15 @@ import voldemort.utils.ClosableIterator;
 import voldemort.utils.EventThrottler;
 import voldemort.utils.Pair;
 import voldemort.versioning.VectorClock;
+import voldemort.versioning.Version;
 import voldemort.versioning.Versioned;
 
+import java.util.List;
 import java.util.Map;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.CopyOnWriteArraySet;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.SynchronousQueue;
@@ -30,21 +38,19 @@ public class AdminClientSlopPusherJob {
     private final static Pair<ByteArray, Versioned<Slop>> END = Pair.create(null, null);
 
     private final Cluster cluster;
-    private final FailureDetector failureDetector;
     private final StoreRepository storeRepo;
     private final Map<Integer, SynchronousQueue<Pair<ByteArray, Versioned<Slop>>>> slopQueues;
     private final ExecutorService consumerExecutor;
     private final EventThrottler writeThrottler;
     private final EventThrottler readThrottler;
     private final AdminClient adminClient;
+    private final CopyOnWriteArraySet<Integer> failedNodes;
 
     public AdminClientSlopPusherJob(Cluster cluster,
-                                    FailureDetector failureDetector,
                                     StoreRepository storeRepo,
                                     long maxReadBytesPerSec,
                                     long maxWriteBytesPerSec) {
         this.cluster = cluster;
-        this.failureDetector = failureDetector;
         this.storeRepo = storeRepo;
         this.slopQueues = Maps.newHashMapWithExpectedSize(cluster.getNumberOfNodes());
         this.consumerExecutor = Executors.newFixedThreadPool(cluster.getNumberOfNodes());
@@ -53,6 +59,7 @@ public class AdminClientSlopPusherJob {
         this.adminClient = new AdminClient(cluster,
                                            new AdminClientConfig().setMaxThreads(cluster.getNumberOfNodes())
                                                                   .setMaxConnectionsPerNode(1));
+        this.failedNodes = new CopyOnWriteArraySet<Integer>();
     }
 
     public void run() {
@@ -74,6 +81,9 @@ public class AdminClientSlopPusherJob {
                         keyAndVal = entries.next();
                         Versioned<Slop> versioned = keyAndVal.getSecond();
                         int nodeId = versioned.getValue().getNodeId();
+                        if(failedNodes.contains(nodeId))
+                            continue;
+                        
                         SynchronousQueue<Pair<ByteArray, Versioned<Slop>>> slopQueue = slopQueues.get(nodeId);
                         if(slopQueue == null) {
                             slopQueue = new SynchronousQueue<Pair<ByteArray, Versioned<Slop>>>();
@@ -101,6 +111,8 @@ public class AdminClientSlopPusherJob {
             } catch(Exception e) {
                 logger.error("Failed to close entries.", e);
             }
+            // Shut down admin client as not to waste connections
+            adminClient.stop();
         }
     }
 
@@ -131,11 +143,19 @@ public class AdminClientSlopPusherJob {
     private class SlopIterator extends AbstractIterator<Pair<String, Pair<ByteArray, Versioned<byte[]>>>> {
 
         private final SynchronousQueue<Pair<ByteArray, Versioned<Slop>>> slopQueue;
-        private volatile int writtenLast;
+        private final Multimap<Integer, Pair<ByteArray, Version>> deleteBatches;
 
-        public SlopIterator(SynchronousQueue<Pair<ByteArray, Versioned<Slop>>> slopQueue) {
+        private final static int BATCH_SIZE = 10000;
+
+        private int writtenLast = 0;
+        private long count = 0L;
+
+        private int currentBatch = 0, prevBatch = -1, oldBatch = -2;
+
+        public SlopIterator(SynchronousQueue<Pair<ByteArray, Versioned<Slop>>> slopQueue,
+                            Multimap<Integer, Pair<ByteArray, Version>> deleteBatches) {
             this.slopQueue = slopQueue;
-            writtenLast = 0;
+            this.deleteBatches = deleteBatches;
         }
 
         @Override
@@ -145,14 +165,21 @@ public class AdminClientSlopPusherJob {
                 boolean shutDown = false;
                 while(!shutDown) {
                     head = slopQueue.poll();
-
-                    if(head == null) {
+                    if(head == null)
                         continue;
-                    }
 
                     if(head.equals(END)) {
                         shutDown = true;
                     } else {
+                        count++;
+                        if(count % BATCH_SIZE == 0) {
+                            currentBatch++;
+                            oldBatch++;
+                            prevBatch++;
+                            if(oldBatch > 0)
+                                deleteOldBatch();
+                        }
+
                         writeThrottler.maybeThrottle(writtenLast);
                         writtenLast = nBytesWritten(head);
                         
@@ -170,20 +197,34 @@ public class AdminClientSlopPusherJob {
             }
 
         }
+
+        private void deleteOldBatch() {
+            for(Pair<ByteArray, Version> key: deleteBatches.get(oldBatch))
+                storeRepo.getSlopStore().delete(key.getFirst(), key.getSecond());
+        }
     }
 
     private class SlopConsumer implements Runnable {
 
         private final int nodeId;
         private final SynchronousQueue<Pair<ByteArray, Versioned<Slop>>> slopQueue;
+        private final Multimap<Integer, Pair<ByteArray, Version>> deleteBatches;
 
         public SlopConsumer(int nodeId,
                             SynchronousQueue<Pair<ByteArray, Versioned<Slop>>> slopQueue) {
             this.nodeId = nodeId;
             this.slopQueue = slopQueue;
+            this.deleteBatches = HashMultimap.create();
         }
+        
         public void run() {
-            adminClient.updateStoreEntries(nodeId, new SlopIterator(slopQueue));
+            try {
+                adminClient.updateStoreEntries(nodeId, new SlopIterator(slopQueue, deleteBatches));
+            } catch (Exception e) {
+                logger.error(e, e);
+                slopQueue.clear();
+                failedNodes.add(nodeId);
+            }
         }
     }
 }
