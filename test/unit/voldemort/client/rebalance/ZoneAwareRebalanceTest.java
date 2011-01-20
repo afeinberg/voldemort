7b4399e402b1b006f2ecba42d244f5ea0596c0ec
Alex Feinberg <alex@strlen.net>
2011-01-20 01:51:40 -0800

    Checkpoint

--- test/unit/voldemort/client/rebalance/ZoneAwareRebalanceTest.java
+++ test/unit/voldemort/client/rebalance/ZoneAwareRebalanceTest.java
@@ -1,8 +1,16 @@
 package voldemort.client.rebalance;
 
+import org.apache.log4j.Logger;
+import org.junit.Before;
 import org.junit.Test;
 import voldemort.ServerTestUtils;
 import voldemort.cluster.Cluster;
+import voldemort.store.StoreDefinition;
+import voldemort.xml.StoreDefinitionsMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -11,6 +19,17 @@ import static org.junit.Assert.*;
  *
  */
 public class ZoneAwareRebalanceTest {
+
+    private final static Logger logger = Logger.getLogger(ZoneAwareRebalanceTest.class);
+    private final static String storeName = "test";
+    private final static String storeDefFile = "test/common/voldemort/config/two-stores.xml";
+
+    private List<StoreDefinition> storeDefList;
+
+    @Before
+    public void setUp() throws IOException {
+        storeDefList = new StoreDefinitionsMapper().readStoreList(new File(storeDefFile));
+    }
 
     @Test
     public void testAddNodeInOneZone() {
@@ -29,6 +48,12 @@ public class ZoneAwareRebalanceTest {
                                                                                        { 10, 7 } },
                                                                          new int[][] { { 0, 1 },
                                                                                        { 2, 3, 4 } });
+        RebalanceClusterPlan clusterPlan = new RebalanceClusterPlan(originalCluster,
+                                                                    targetCluster,
+                                                                    storeDefList,
+                                                                    true,
+                                                                    null);
+        logger.info("RebalanceClusterPlan: " + clusterPlan);
     }
 
     @Test
@@ -59,7 +84,7 @@ public class ZoneAwareRebalanceTest {
                                                                                          { 5, 7, 11 } },
                                                                            new int[][] { { 0, 1 },
                                                                                          { 2, 3 } });
-         Cluster targetCluster = ServerTestUtils.getLocalClusterWithZones(5,
+         Cluster targetCluster = ServerTestUtils.getLocalClusterWithZones(6,
                                                                           new int[][] { { 0, 9 },
                                                                                         { 1, 3 },
                                                                                         { 4, 6 },
