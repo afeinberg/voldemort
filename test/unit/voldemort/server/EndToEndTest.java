dfac4b16feae7410febbf1f1da6a4c574ec46035
Alex Feinberg <alex@strlen.net>
2011-02-25 18:43:41 -0800

    Replicate a test situation where BDB fails to read a result on read after a put

--- test/unit/voldemort/server/EndToEndTest.java
+++ test/unit/voldemort/server/EndToEndTest.java
@@ -10,8 +10,13 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -28,6 +33,7 @@ import voldemort.cluster.Cluster;
 import voldemort.cluster.Node;
 import voldemort.store.socket.SocketStoreFactory;
 import voldemort.store.socket.clientrequest.ClientRequestExecutorPool;
+import voldemort.versioning.ObsoleteVersionException;
 import voldemort.versioning.Versioned;
 
 /**
@@ -62,6 +68,10 @@ public class EndToEndTest {
     public void setUp() throws IOException {
         cluster = ServerTestUtils.getLocalCluster(2, new int[][] { { 0, 2, 4, 6 }, { 1, 3, 5, 7 } });
         servers = new ArrayList<VoldemortServer>();
+        Properties serverProps = new Properties();
+        serverProps.put("max.threads", "100");
+        serverProps.put("bdb.fair.latches", "true");
+
         servers.add(ServerTestUtils.startVoldemortServer(socketStoreFactory,
                                                          ServerTestUtils.createServerConfig(useNio,
                                                                                             0,
@@ -69,7 +79,7 @@ public class EndToEndTest {
                                                                                                      .getAbsolutePath(),
                                                                                             null,
                                                                                             STORES_XML,
-                                                                                            new Properties()),
+                                                                                            serverProps),
                                                          cluster));
         servers.add(ServerTestUtils.startVoldemortServer(socketStoreFactory,
                                                          ServerTestUtils.createServerConfig(useNio,
@@ -78,7 +88,7 @@ public class EndToEndTest {
                                                                                                      .getAbsolutePath(),
                                                                                             null,
                                                                                             STORES_XML,
-                                                                                            new Properties()),
+                                                                                            serverProps),
                                                          cluster));
         Node node = cluster.getNodeById(0);
         String bootstrapUrl = "tcp://" + node.getHost() + ":" + node.getSocketPort();
@@ -118,5 +128,43 @@ public class EndToEndTest {
 
         storeClient.delete("Ukraine");
         assertNull("delete works as expected", storeClient.get("Ukraine"));
+    }
+
+    @Test
+    public void testConcurrentReadsAfterPuts() throws Exception {
+        Node node = cluster.getNodeById(0);
+        String bootstrapUrl = "tcp://" + node.getHost() + ":" + node.getSocketPort();
+        final StoreClientFactory storeClientFactory = new SocketStoreClientFactory(new ClientConfig().setBootstrapUrls(bootstrapUrl)
+                                                                                                     .setEnablePipelineRoutedStore(true)
+                                                                                                     .setSelectors(1));
+        StoreClient<String, String> storeClient = storeClientFactory.getStoreClient("test-highvolume-persistent");
+        storeClient.put("a", "0");
+
+        ExecutorService executor = Executors.newFixedThreadPool(10);
+        final AtomicBoolean failed = new AtomicBoolean(false);
+        for(int i = 0; i < 10; i++) {
+            executor.submit(new Runnable() {
+                public void run() {
+                    StoreClient<String, String> otherClient = storeClientFactory.getStoreClient("test-highvolume-persistent");
+                    for(int i = 0; i < 1000 && !failed.get(); i++) {
+
+                        Versioned<String> versioned = otherClient.get("a");
+                        if(versioned == null)
+                            failed.set(true);
+                        else {
+                            versioned.setObject(Integer.toString(Integer.parseInt(versioned.getValue()) + 1));
+                            try {
+                                otherClient.put("a", versioned);
+                            } catch(ObsoleteVersionException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }
+            });
+        }
+        executor.shutdown();
+        executor.awaitTermination(60, TimeUnit.SECONDS);
+        Assert.assertFalse("there should not have been any nulls", failed.get());
     }
 }
