7aa02d763eef1ba5913596a4f86d163c6886530d
Alex Feinberg <alex@strlen.net>
2011-05-06 19:00:49 -0700

    Add quota verification job

---
+++ src/java/voldemort/server/scheduler/quota/QuotaVerificationJob.java
@@ -0,0 +1,30 @@
+package voldemort.server.scheduler.quota;
+
+
+import org.apache.log4j.Logger;
+import voldemort.store.quota.AbstractQuotaEnforcingStore;
+
+public class QuotaVerificationJob<K, V, T> implements Runnable {
+
+    private final static Logger logger = Logger.getLogger(QuotaVerificationJob.class);
+
+    private final AbstractQuotaEnforcingStore<K, V, T> store;
+    private final String description;
+
+    public QuotaVerificationJob(AbstractQuotaEnforcingStore<K, V, T> store,
+                                String description) {
+        this.store = store;
+        this.description = description;
+    }
+
+    public void run() {
+        logger.info("Starting " + description + " quota verification on store \""
+                    + store.getName() + "\"...");
+        try {
+            store.verifyQuota();
+        } catch(Exception e) {
+            logger.error("Error running verification job", e);
+        }
+        logger.info("Quota verification finished on store \"" + store.getName() + "\".");
+    }
+}
