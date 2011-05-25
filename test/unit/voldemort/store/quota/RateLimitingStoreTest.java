4aa630b0a688005bb3d333518469c301ee0e743b
Alex Feinberg <alex@strlen.net>
2011-05-24 21:50:56 -0700

    Throughput calculated correctly

---
+++ test/unit/voldemort/store/quota/RateLimitingStoreTest.java
@@ -0,0 +1,51 @@
+package voldemort.store.quota;
+
+import org.apache.log4j.Logger;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import voldemort.store.StorageEngine;
+import voldemort.store.Store;
+import voldemort.versioning.Versioned;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+public class RateLimitingStoreTest {
+
+    private static final Logger logger = Logger.getLogger(RateLimitingStoreTest.class);
+
+    private final Quota quota = new Quota(100, 1000);
+
+    @Mock
+    private StorageEngine<String, String, Void> mockEngine;
+    @Mock
+    private QuotaAction action;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        when(mockEngine.get("foo", null)).thenReturn(Arrays.asList(Versioned.value("bar")));
+    }
+
+    @Test
+    public void testGetThroughput() throws Exception {
+        RateLimitingStore<String, String, Void> rlStore = getLimitingStore(mockEngine,
+                                                                           quota,
+                                                                           action);
+        for(int i = 0; i < 10000; i++)
+            rlStore.put("foo", Versioned.value("bar"), null);
+        assertTrue(rlStore.getThroughput() >= 1000);
+    }
+
+    private <K, V, T> RateLimitingStore<K, V, T> getLimitingStore(Store<K, V, T> store,
+                                                                  Quota quota,
+                                                                  QuotaAction action) {
+        return new RateLimitingStore<K, V, T>(store, quota, action, 1000);
+    }
+
+
+}
