1560630b989464f82526b031f15095e262294161
Alex Feinberg <alex@strlen.net>
2011-05-31 16:19:28 -0700

    Integration testing work

--- test/integration/voldemort/store/quota/RateLimitIntegrationTest.java
+++ test/integration/voldemort/store/quota/RateLimitIntegrationTest.java
@@ -3,8 +3,20 @@ package voldemort.store.quota;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import voldemort.ServerTestUtils;
+import voldemort.TestUtils;
+import voldemort.client.ClientConfig;
+import voldemort.client.SocketStoreClientFactory;
+import voldemort.client.StoreClient;
+import voldemort.client.StoreClientFactory;
+import voldemort.cluster.Cluster;
+import voldemort.server.VoldemortConfig;
+import voldemort.server.VoldemortServer;
+import voldemort.store.socket.SocketStoreFactory;
+import voldemort.store.socket.clientrequest.ClientRequestExecutorPool;
 
 import java.io.IOException;
+import java.util.Properties;
 
 import static org.junit.Assert.*;
 
@@ -13,10 +25,43 @@ public class RateLimitIntegrationTest {
     private final static String STORES_XML_FILE = "test/common/voldemort/config/stores.xml";
     private static final String STORE_NAME = "test-rate-limit";
 
+    private final SocketStoreFactory socketStoreFactory = new ClientRequestExecutorPool(2,
+                                                                                        10000,
+                                                                                        100000,
+                                                                                        32 * 1024);
+    private VoldemortServer servers[] = new VoldemortServer[2];
+    private VoldemortConfig configs[] = new VoldemortConfig[2];
+    private Cluster cluster;
+    private StoreClient<String, String> client;
 
     @Before
     public void setUp() throws IOException {
-        // TODO: Setup a two node cluster
+        for(int i = 0; i < 2; i++) {
+            configs[i] =  ServerTestUtils.createServerConfig(false,
+                                                             i,
+                                                             TestUtils.createTempDir()
+                                                                      .getAbsolutePath(),
+                                                             null,
+                                                             STORES_XML_FILE,
+                                                             new Properties());
+        }
+        for(VoldemortConfig config: configs) {
+            config.setEnableQuota(true);
+            config.setEnforceQuota(true);
+        }
+
+        cluster = ServerTestUtils.getLocalCluster(2, new int [][] { { 0, 2 },  { 1, 3 } });
+
+        for(int i = 0; i < 2; i++) {
+            servers[i] = ServerTestUtils.startVoldemortServer(socketStoreFactory,
+                                                              configs[i],
+                                                              cluster);
+        }
+
+        String bootstrapUrl = cluster.getNodeById(0).getSocketUrl().toString();
+        ClientConfig config = new ClientConfig().setBootstrapUrls(bootstrapUrl);
+        StoreClientFactory factory = new SocketStoreClientFactory(config);
+        client = factory.getStoreClient(STORE_NAME);
     }
 
     @After
