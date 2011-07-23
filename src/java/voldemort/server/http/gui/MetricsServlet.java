39aec3d2dec8c0c23f4fadc18e74400ce761a2ad
Alex Feinberg <alex@strlen.net>
2011-07-22 18:01:52 -0700

    Skeleton for the servlet

--- src/java/voldemort/server/http/gui/MetricsServlet.java
+++ src/java/voldemort/server/http/gui/MetricsServlet.java
@@ -1,6 +1,7 @@
 package voldemort.server.http.gui;
 
 import voldemort.annotations.Experimental;
+import voldemort.metrics.SensorRegistry;
 import voldemort.server.VoldemortServer;
 import voldemort.server.http.VoldemortServletContextListener;
 import voldemort.utils.Utils;
@@ -33,8 +34,22 @@ public class MetricsServlet extends HttpServlet {
     }
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        String sensorDomain = request.getParameter("domain");
+        String sensorType = request.getParameter("type");
+        SensorRegistry registry = server.getSensorRegistry();
 
+        Object sensorObject = registry.getSensor(sensorDomain, sensorType);
+        if(sensorObject == null) {
+            response.sendError(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        outputJSON(response, sensorObject);
+    }
+
+    protected void outputJSON(HttpServletResponse response, Object sensorObject) {
+        // TODO: examine annotations, invoke methods and print the object
     }
 }
